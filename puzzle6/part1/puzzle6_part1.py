bd7e1a8fa4a8dcd3b533e8949f428f2d80d65c20
Daniel Morton <daniel@djmorton.com>
2016-12-06 23:23:24 -0500

    Added puzzle 6 parts 1 and 2

---
+++ puzzle6/part1/puzzle6_part1.py
@@ -0,0 +1,25 @@
+import operator
+
+positional_histograms = None
+
+recovered_message = ''
+
+with open('input.txt') as file_handle:
+    for line in file_handle:
+        if positional_histograms is None:
+            positional_histograms = []
+            for _ in range(len(line.strip())):
+                positional_histograms.append({})
+
+        for index, char in enumerate(line.strip()):
+            histogram = positional_histograms[index]    
+            if char in histogram:
+                histogram[char] += 1
+            else:
+                histogram[char] = 1
+
+
+    for histogram in positional_histograms:
+        recovered_message += max(histogram.iteritems(), key=operator.itemgetter(1))[0]
+
+print "Message: " + recovered_message
